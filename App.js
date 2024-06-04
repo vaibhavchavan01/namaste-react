2c1ced0023feed41ac96b9f2f6b02296bdf1ec0a
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2024-06-04 12:25:51 +0530

    Episode-01 | inception

---
+++ App.js
@@ -0,0 +1,22 @@
+// const heading = React.createElement("h1", {}, "Hello World from react!");
+const parent = React.createElement(
+    "dev",
+    { id: "parent" },
+    React.createElement(
+        "dev",
+        { id: "child" },
+        [React.createElement(
+            "h1",
+            {},
+            "Im in h1 tag"
+        ),
+        React.createElement(
+            "h2",
+            {},
+            "Im in h2 tag"  
+        )]
+    )
+)
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
