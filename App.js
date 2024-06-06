709d372583890084244bc8e048164458b7eb8d89
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2024-06-06 16:01:38 +0530

    E3-Lying the foundation.

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
-// const heading = React.createElement("h1", {}, "Hello World from react!");
-const parent = React.createElement(
-    "dev",
-    { id: "parent" },
-    React.createElement(
-        "dev",
-        { id: "child" },
-        [React.createElement(
-            "h1",
-            {},
-            "Im in h1 tag"
-        ),
-        React.createElement(
-            "h2",
-            {},
-            "Im in h2 tag"  
-        )]
-    )
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const title = (
+    <h1>this is title element</h1>
 )
-console.log(parent);
+
+const functionComponent = () => (
+    <div>
+        {title}
+    </div>
+)
+// const heading = React.createElement("h1", {}, "Hello World from react!");
+// const parent = React.createElement(
+//     "dev",
+//     { id: "parent" },
+//     React.createElement(
+//         "dev",
+//         { id: "child" },
+//         [React.createElement(
+//             "h1",
+//             {},
+//             "Im in h1 tag"
+//         ),
+//         React.createElement(
+//             "h2",
+//             {},
+//             "Im in h2 tag"  
+//         )]
+//     )
+// )
+// console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+root.render(functionComponent)
